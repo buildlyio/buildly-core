1b0b04f61d7136a15f69b84ef125e5e367d55e40
Jeferson Moura <jefmmoreira@gmail.com>
2018-09-26 14:08:11 +0200

    Add LogicModule model

--- workflow/models.py
+++ workflow/models.py
@@ -98,6 +98,29 @@ class Organization(models.Model):
         return self.name
 
 
+class LogicModule(models.Model):
+    module_uuid = models.CharField(max_length=255, verbose_name='Logic Module UUID', default=uuid.uuid4, unique=True)
+    name = models.CharField("Logic Module Name", max_length=255, blank=True)
+    description = models.TextField("Description/Notes", max_length=765, null=True, blank=True)
+    endpoint = models.CharField(blank=True, null=True, max_length=255)
+    github_repo = models.CharField(blank=True, null=True, max_length=500)
+    create_date = models.DateTimeField(null=True, blank=True)
+    edit_date = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = "Logic Modules"
+
+    def save(self, *args, **kwargs):
+        if self.create_date is None:
+            self.create_date = timezone.now()
+        self.edit_date = timezone.now()
+        super(LogicModule, self).save()
+
+    def __unicode__(self):
+        return self.name
+
+
 class Country(models.Model):
     country = models.CharField("Country Name", max_length=255, blank=True, help_text="ISO Country Name")
     code = models.CharField("2 Letter Country Code", max_length=4, blank=True, help_text="ISO Country Code")
