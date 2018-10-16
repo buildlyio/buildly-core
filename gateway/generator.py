8cebe483bf158315e63b0ad33573293e401e9c08
Jeferson Moura <jefmmoreira@gmail.com>
2018-10-16 17:06:15 +0200

    Create a OpenAPISchemaGenerator for the API gateway

---
+++ gateway/generator.py
@@ -0,0 +1,45 @@
+from drf_yasg import generators as drf_gen
+from drf_yasg import openapi
+
+from . import aggregator
+from . import utils
+
+
+class OpenAPISchemaGenerator(drf_gen.OpenAPISchemaGenerator):
+    def get_schema(self, request=None, public=False):
+        schema_urls = utils.get_swagger_urls()
+        config_aggregator = {
+            'info': {
+                'title': 'API Gateway',
+                'description': '',
+                'version': '1.0'
+            },
+            'apis': schema_urls,
+            'produces': ['application/json'],
+            'consumes': ['application/json']
+        }
+        sw_aggregator = aggregator.SwaggerAggregator(config_aggregator)
+        swagger_spec = sw_aggregator.generate_swagger()
+
+        endpoints = self.get_endpoints(request)
+        components = openapi.ReferenceResolver(openapi.SCHEMA_DEFINITIONS)
+        paths, prefix = self.get_paths(endpoints, components, request, public)
+        paths.update(swagger_spec['paths'])
+
+        url = self.url
+        if url is None and request is not None:
+            url = request.build_absolute_uri()
+
+        return openapi.Swagger(
+            info=self.info,
+            paths=paths,
+            consumes=swagger_spec['consumes'],
+            produces=swagger_spec['produces'],
+            security_definitions=swagger_spec.get(
+                'security_definitions', None),
+            security=swagger_spec.get('security', None),
+            definitions=swagger_spec['definitions'],
+            _url=url,
+            _version=self.version,
+            _prefix=prefix
+        )
