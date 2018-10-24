6d3a805441f7d95b86b8215ef3889353b384c1a8
Jeferson Moura <jefmmoreira@gmail.com>
2018-10-24 09:50:11 +0200

    Remove unneeded email settings

--- bifrost-api/settings/production.py
+++ bifrost-api/settings/production.py
@@ -54,23 +54,3 @@ LOGGING = {
         },
     },
 }
-
-# Email settings
-# https://docs.djangoproject.com/en/1.11/topics/email/
-
-if os.getenv('EMAIL_BACKEND') == 'SMTP':
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = os.environ['EMAIL_HOST']
-    EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
-    EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
-    EMAIL_PORT = os.getenv('EMAIL_PORT', 587)
-    EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', True)
-    EMAIL_SUBJECT_PREFIX = os.getenv('EMAIL_SUBJECT_PREFIX', '')
-else:
-    warnings.warn('No SMTP email backend provider. Falling back to dummy '
-                  'backend')
-    EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
-
-DEFAULT_FROM_EMAIL = os.environ['DEFAULT_FROM_EMAIL']
-DEFAULT_REPLY_TO = os.environ['DEFAULT_REPLY_TO']
-SALES_TEAM_EMAIL = os.getenv('SALES_TEAM_EMAIL', '')
