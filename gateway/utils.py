779482d7066ce3134feb088f349cb87056084d0a
Jeferson Moura <jefmmoreira@gmail.com>
2019-01-11 15:09:40 +0100

    Update service name loopkup

--- gateway/utils.py
+++ gateway/utils.py
@@ -1,3 +1,4 @@
+import re
 import requests
 
 from . import exceptions
@@ -8,20 +9,21 @@ SWAGGER_LOOKUP_FORMAT = 'json'
 SWAGGER_LOOKUP_PATH = 'docs'
 
 
-def get_swagger_urls(service: str=None) -> dict:
+def get_swagger_urls(service: str = None) -> dict:
     """
     Get the endpoint of the service in the database and append
     with the OpenAPI path
 
-    :param service: the name of the service
-    :return: the url to fetch the openapi schema
+    :param str service: the name of the service
+    :return: dict
+             Key-value pair with service name and OpenAPI schema URL of it
     """
     if service is None:
         modules = gtm.LogicModule.objects.values(
             'name', 'endpoint').all()
     else:
         modules = gtm.LogicModule.objects.values(
-            'name', 'endpoint').filter(name__iexact=service)
+            'name', 'endpoint').filter(name__istartswith=service)
 
     if len(modules) == 0 and service is not None:
         msg = 'Service "{}" not found.'.format(service)
@@ -33,7 +35,7 @@ def get_swagger_urls(service: str=None) -> dict:
             module['endpoint'], SWAGGER_LOOKUP_PATH,
             SWAGGER_LOOKUP_FIELD, SWAGGER_LOOKUP_FORMAT
         )
-        module_name = module['name'].lower().split('_')[0]
+        module_name = re.sub('_service$', '', module['name'].lower())
         module_urls[module_name] = swagger_url
 
     return module_urls
