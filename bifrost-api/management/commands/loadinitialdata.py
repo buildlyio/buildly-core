1877f80c2ccb315680391ca6fe0ec6ec1cddf6e7
Jeferson Moura <jefmmoreira@gmail.com>
2019-01-17 14:35:25 +0100

    Add groups to loadinitialdata

--- bifrost-api/management/commands/loadinitialdata.py
+++ bifrost-api/management/commands/loadinitialdata.py
@@ -5,6 +5,8 @@ from django.core.management.base import BaseCommand
 from django.db import transaction
 
 import factories
+from workflow.models import (ROLE_VIEW_ONLY, ROLE_ORGANIZATION_ADMIN,
+                             ROLE_PROGRAM_ADMIN, ROLE_PROGRAM_TEAM)
 
 logger = logging.getLogger(__name__)
 
@@ -20,6 +22,7 @@ class Command(BaseCommand):
         # Note: for the lists we fill the first element with an empty value for
         # development readability (id == position).
         self._application = None
+        self._groups = ['']
 
     def _create_oauth_application(self):
         if settings.OAUTH_CLIENT_ID is not None:
@@ -29,7 +32,28 @@ class Command(BaseCommand):
                 client_id=settings.OAUTH_CLIENT_ID,
             )
 
+    def _create_groups(self):
+        self._groups.append(factories.Group(
+            id=1,
+            name=ROLE_VIEW_ONLY,
+        ))
+
+        self._groups.append(factories.Group(
+            id=2,
+            name=ROLE_ORGANIZATION_ADMIN,
+        ))
+
+        self._groups.append(factories.Group(
+            id=3,
+            name=ROLE_PROGRAM_ADMIN,
+        ))
+
+        self._groups.append(factories.Group(
+            id=4,
+            name=ROLE_PROGRAM_TEAM,
+        ))
 
     @transaction.atomic
     def handle(self, *args, **options):
+        self._create_groups()
         self._create_oauth_application()
