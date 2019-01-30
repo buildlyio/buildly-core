e4eb7b2a6b166e48707474e266471ea63cf9be9b
Jeferson Moura <jefmmoreira@gmail.com>
2019-01-30 10:58:45 +0100

    Add workflow models factory
    Conflicts:
    	factories/workflow_models.py

--- factories/workflow_models.py
+++ factories/workflow_models.py
@@ -2,13 +2,16 @@ from factory import DjangoModelFactory, LazyAttribute, SubFactory
 
 from workflow.models import (
     CoreUser as CoreUserM,
-    Organization as OrganizationM
+    Organization as OrganizationM,
+    WorkflowLevel1 as WorkflowLevel1M,
+    WorkflowLevel2 as WorkflowLevel2M
 )
 
 
 class Organization(DjangoModelFactory):
     class Meta:
         model = OrganizationM
+        django_get_or_create = ('name',)
 
     name = 'Default Organization'
 
@@ -20,3 +23,18 @@ class CoreUser(DjangoModelFactory):
 
     name = LazyAttribute(lambda o: o.user.first_name + " " + o.user.last_name)
     organization = SubFactory(Organization)
+
+
+class WorkflowLevel1(DjangoModelFactory):
+    class Meta:
+        model = WorkflowLevel1M
+
+    name = 'Health and Survival for Syrians in Affected Regions'
+
+
+class WorkflowLevel2(DjangoModelFactory):
+    class Meta:
+        model = WorkflowLevel2M
+
+    name = 'Help Syrians'
+    workflowlevel1 = SubFactory(WorkflowLevel1)
