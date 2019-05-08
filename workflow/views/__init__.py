d6d943ea2dbed8626f6b56c6b279cba5b1a27192
Nikolay Kovalenko <nikolay.kovalenko@humanitec.com>
2019-05-08 15:13:00 +0200

    PLAT-394: remove DRFAuthenticatedGraphQLView

--- workflow/views/__init__.py
+++ workflow/views/__init__.py
@@ -1,5 +1,3 @@
-from graphene_django.views import GraphQLView
-
 from .coregroup import CoreGroupViewSet  # noqa
 from .coreuser import CoreUserViewSet  # noqa
 from .i18n import InternationalizationViewSet  # noqa
@@ -7,16 +5,3 @@ from .organization import OrganizationViewSet  # noqa
 from .workflowlevel1 import WorkflowLevel1ViewSet  # noqa
 from .workflowlevel2 import WorkflowLevel2ViewSet, WorkflowLevel2SortViewSet  # noqa
 from .workflowteam import WorkflowTeamViewSet  # noqa
-
-
-# TODO: do we need this?
-"""
-GraphQL views from Graphene
-"""
-
-
-class DRFAuthenticatedGraphQLView(GraphQLView):
-    @classmethod
-    def as_view(cls, *args, **kwargs):
-        view = super(GraphQLView, cls).as_view(*args, **kwargs)
-        return view
