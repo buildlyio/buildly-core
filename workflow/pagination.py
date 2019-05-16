7423f861f4ecf1b53ab8a1e98b05a5d8fbfd991d
Ralf Kostulski <639859+ralfzen@users.noreply.github.com>
2019-05-16 12:28:24 +0200

    Add Pagination ToDo Comment about env vars

--- workflow/pagination.py
+++ workflow/pagination.py
@@ -14,10 +14,6 @@ class SmallResultsSetPagination(PageNumberPagination):
 
 
 class DefaultCursorPagination(CursorPagination):
-    """
-    TODO move this to settings to provide better standardization
-    See http://www.django-rest-framework.org/api-guide/pagination/
-    """
     page_size = 30
     max_page_size = 100
     page_size_query_param = 'page_size'
