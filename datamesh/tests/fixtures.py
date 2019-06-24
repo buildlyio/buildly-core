43b847f428304361256ab45d199ef2424d52232e
Nikolay Kovalenko <nikolay.kovalenko@humanitec.com>
2019-06-24 18:27:40 +0200

    PLAT-465: test for list aggregation

--- datamesh/tests/fixtures.py
+++ datamesh/tests/fixtures.py
@@ -1,4 +1,8 @@
+import uuid
+from unittest.mock import Mock
+
 import pytest
+from pyswagger.io import Response as PySwaggerResponse
 
 import factories
 
@@ -11,12 +15,45 @@ def join_record():
 @pytest.fixture
 def relationship():
     lm = factories.LogicModule(name='Products Service', endpoint_name='products')
-    lm_document = factories.LogicModule(name='Document Service', endpoint_name='documents')
     lmm = factories.LogicModuleModel(logic_module=lm, model='Product', endpoint='/products/')
+    lm_document = factories.LogicModule(name='Document Service', endpoint_name='documents')
     lmm_document = factories.LogicModuleModel(logic_module=lm_document, model='Document', endpoint='/documents/')
     return factories.Relationship(origin_model=lmm, related_model=lmm_document, key='document_relationship')
 
 
 @pytest.fixture
+def relationship2(relationship):
+    lmm = relationship.origin_model  # Products model from 1st relationship
+    lm_location = factories.LogicModule(name='Location Service', endpoint_name='location')
+    lmm_location = factories.LogicModuleModel(logic_module=lm_location, model='Location', endpoint='/siteprofile/')
+    return factories.Relationship(origin_model=lmm, related_model=lmm_location, key='location_relationship')
+
+
+@pytest.fixture
+def relationship_with_10_records():
+    lm = factories.LogicModule(name='Products Service', endpoint_name='products')
+    lmm = factories.LogicModuleModel(logic_module=lm, model='Product', endpoint='/products/', lookup_field_name='uuid')
+    lm_document = factories.LogicModule(name='Document Service', endpoint_name='documents')
+    lmm_document = factories.LogicModuleModel(logic_module=lm_document, model='Document', endpoint='/documents/',
+                                              lookup_field_name='uuid')
+    relationship = factories.Relationship(origin_model=lmm, related_model=lmm_document, key='document_relationship')
+    for _ in range(10):
+        factories.JoinRecord.create(relationship=relationship,
+                                    record_uuid=uuid.uuid4(), record_id=None,
+                                    related_record_uuid=uuid.uuid4(), related_record_id=None)
+    return relationship
+
+
+@pytest.fixture
 def logic_module_model():
     return factories.LogicModuleModel()
+
+
+def service_response_mock(data):
+    headers = {'Content-Type': ['application/json']}
+    service_response = Mock(PySwaggerResponse)
+    service_response.status = 200
+    service_response.header = headers
+    service_response.data = data
+    return service_response
+
--- datamesh/tests/test_join.py
+++ datamesh/tests/test_join.py
@@ -3,56 +3,128 @@ from unittest.mock import Mock, patch
 
 import pytest
 from pyswagger import App
-from pyswagger.io import Response as PySwaggerResponse
 
 import factories
-from datamesh.tests.fixtures import relationship
+from datamesh.tests.fixtures import relationship, relationship2, relationship_with_10_records, service_response_mock
 from workflow.tests.fixtures import auth_api_client
 
 
 @pytest.mark.django_db()
-class TestDataMesh:
+@patch('gateway.views.APIGatewayView._load_swagger_resource')
+@patch('gateway.views.APIGatewayView._perform_service_request')
+def test_join_data_one_obj_w_relationships(mock_perform_request, mock_app, auth_api_client, relationship):
+    factories.JoinRecord(relationship=relationship, record_id=1, related_record_id=2)
+    # mock app
+    mock_app.return_value = Mock(App)
 
-    @patch('gateway.views.APIGatewayView._load_swagger_resource')
-    @patch('gateway.views.APIGatewayView._perform_service_request')
-    def test_join_data_one_obj_w_relationships(self, mock_perform_request, mock_app, auth_api_client, relationship):
-        factories.JoinRecord(relationship=relationship, record_id=1, related_record_id=2)
-        # mock app
-        mock_app.return_value = Mock(App)
+    # mock first response
+    service_response = service_response_mock({
+        'id': 1,
+        'name': 'test',
+        'contact_uuid': 1
+    })
+    # mock second response
+    expand_response = service_response_mock({
+        'id': 1,
+        'file': '/somewhere/128/',
+    })
+    mock_perform_request.side_effect = [service_response, expand_response]
 
-        # mock first response
-        headers = {'Content-Type': ['application/json']}
-        service_response = Mock(PySwaggerResponse)
-        service_response.status = 200
-        service_response.data = {
-            'id': 1,
-            'name': 'test',
-            'contact_uuid': 1
-        }
-        service_response.header = headers
-        # mock second response
-        expand_data = {
+    # make api request
+    path = '/{}/{}/'.format(relationship.origin_model.logic_module.endpoint_name, 'products')
+    response = auth_api_client.get(path, {'join': ''})
+
+    # validate result
+    expected_data = {
+        'id': 1,
+        'name': 'test',
+        'contact_uuid': 1,
+        relationship.key: [{
             'id': 1,
             'file': '/somewhere/128/',
-        }
-        expand_response = Mock(PySwaggerResponse)
-        expand_response.data = expand_data
-        mock_perform_request.side_effect = [service_response, expand_response]
+        }]
+    }
 
-        # make api request
-        path = '/{}/{}/'.format(relationship.origin_model.logic_module.endpoint_name, 'products')
-        response = auth_api_client.get(path, {'join': ''})
+    assert response.status_code == 200
+    assert json.loads(response.content) == expected_data
 
-        # validate result
-        expected_data = {
-            'id': 1,
-            'name': 'test',
-            'contact_uuid': 1,
-            'document_relationship': [{
-                'id': 1,
-                'file': '/somewhere/128/',
-            }]
-        }
 
-        assert response.status_code == 200
-        assert json.loads(response.content) == expected_data
+@pytest.mark.django_db()
+@patch('gateway.views.APIGatewayView._load_swagger_resource')
+@patch('gateway.views.APIGatewayView._perform_service_request')
+def test_join_data_one_obj_w_two_relationships(mock_perform_request, mock_app, auth_api_client,
+                                               relationship, relationship2):
+    factories.JoinRecord(relationship=relationship, record_id=1, related_record_id=2)
+    factories.JoinRecord(relationship=relationship2, record_id=1, related_record_id=10)
+
+    mock_app.return_value = Mock(App)
+    # mock first response
+    service_response = service_response_mock({
+        'id': 1,
+        'name': 'test',
+        'contact_uuid': 1
+    })
+    # mock second response
+    expand_response1 = service_response_mock({
+        'id': 2,
+        'file': '/documents/128/',
+    })
+    # mock third response
+    expand_response2 = service_response_mock({
+        'id': 10,
+        'city': 'New York',
+    })
+    mock_perform_request.side_effect = [service_response, expand_response1, expand_response2]
+
+    # make api request
+    path = '/{}/{}/'.format(relationship.origin_model.logic_module.endpoint_name, 'products')
+    response = auth_api_client.get(path, {'join': ''})
+
+    # validate result
+    expected_data = {
+        'id': 1,
+        'name': 'test',
+        'contact_uuid': 1,
+        relationship.key: [{
+            'id': 2,
+            'file': '/documents/128/',
+        }],
+        relationship2.key: [{
+            'id': 10,
+            'city': 'New York',
+        }]
+    }
+
+    assert response.status_code == 200
+    assert json.loads(response.content) == expected_data
+
+
+@pytest.mark.django_db()
+@patch('gateway.views.APIGatewayView._load_swagger_resource')
+@patch('gateway.views.APIGatewayView._perform_service_request')
+def test_join_data_list(mock_perform_request, mock_app, auth_api_client, relationship_with_10_records):
+    mock_app.return_value = Mock(App)
+
+    join_records = relationship_with_10_records.joinrecords.all()
+    main_service_data = [{'uuid': item.record_uuid, 'name': f'Boiler #{i}'}
+                         for i, item in enumerate(join_records)]
+    main_service_response = service_response_mock(main_service_data)
+
+    expand_responses = []
+    for item in join_records:
+        expand_responses.append(
+            service_response_mock({'uuid': item.related_record_uuid, 'file': '/documents/128/'})
+        )
+    mock_perform_request.side_effect = [main_service_response] + expand_responses
+
+    # make api request
+    path = '/{}/{}/'.format(relationship_with_10_records.origin_model.logic_module.endpoint_name, 'products')
+    response = auth_api_client.get(path, {'join': ''})
+
+    assert response.status_code == 200
+    for i, item in enumerate(json.loads(response.content)):
+        assert item['uuid'] == str(join_records[i].record_uuid)
+        assert relationship_with_10_records.key in item
+        nested = item[relationship_with_10_records.key]
+        assert len(nested) == 1
+        assert nested[0]['uuid'] == str(join_records[i].related_record_uuid)
