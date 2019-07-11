fd5a8821476032bcfa514e1a0704b5936041b6e8
Katyanna Moura <amelie.kn@gmail.com>
2019-07-11 15:46:10 +0200

    CU-761 Add `id` to serializers

--- workflow/tests/test_workflowlevel2serializers.py
+++ workflow/tests/test_workflowlevel2serializers.py
@@ -6,37 +6,6 @@ import factories
 from ..serializers import WorkflowLevel2NameSerializer, WorkflowLevel2Serializer
 
 
-class Workflowlevel2SerializerTest(TestCase):
-    def setUp(self):
-        self.factory = APIRequestFactory()
-
-    def test_contains_expected_fields(self):
-        wfl2 = factories.WorkflowLevel2()
-
-        serializer = WorkflowLevel2Serializer(instance=wfl2)
-
-        data = serializer.data
-
-        keys = [
-            'name',
-            'level2_uuid',
-            'workflowlevel1',
-            'short_name',
-            'description',
-            'type',
-            'notes',
-            'parent_workflowlevel2',
-            'start_date',
-            'end_date',
-            'core_groups',
-            'edit_date',
-            'create_date',
-            'created_by',
-        ]
-
-        self.assertEqual(set(data.keys()), set(keys))
-
-
 class WorkflowLevel2NameSerializerTest(TestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
