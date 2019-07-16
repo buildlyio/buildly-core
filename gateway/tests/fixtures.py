f3773561ecbecd5fcb92e60767f843960fe70c29
Nikolay Kovalenko <nikolay.kovalenko@humanitec.com>
2019-07-16 14:32:18 +0200

    fix test fixtures after rebase

--- gateway/tests/fixtures.py
+++ gateway/tests/fixtures.py
@@ -16,9 +16,9 @@ def datamesh():
                                        endpoint='http://locationservice:8080')
     lm2 = factories.LogicModule.create(name='documents', endpoint_name='documents',
                                        endpoint='http://documentservice:8080')
-    lmm1 = factories.LogicModuleModel(logic_module=lm1, model='SiteProfile', endpoint='/siteprofiles/',
-                                      lookup_field_name='uuid')
-    lmm2 = factories.LogicModuleModel(logic_module=lm2, model='Document', endpoint='/documents/',
-                                      lookup_field_name='id')
+    lmm1 = factories.LogicModuleModel(logic_module_endpoint_name=lm1.endpoint_name, model='SiteProfile',
+                                      endpoint='/siteprofiles/', lookup_field_name='uuid')
+    lmm2 = factories.LogicModuleModel(logic_module_endpoint_name=lm2.endpoint_name, model='Document',
+                                      endpoint='/documents/', lookup_field_name='id')
     relationship = factories.Relationship(origin_model=lmm1, related_model=lmm2, key='documents')
     return lm1, lm2, relationship
