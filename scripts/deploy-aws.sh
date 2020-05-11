3e719c8092f2ab32f09375a739b9bc09a2a0dd2f
Greg Lind <gwlind@gmail.com>
2020-05-11 12:19:10 -0400

    update travis for aws

--- scripts/deploy-aws.sh
+++ scripts/deploy-aws.sh
@@ -3,8 +3,7 @@
 # Push image to ECR
 ###################
 pip install awscli
-aws ecr get-login-password --region us-west-2
-docker login --username AWS --password-stdin 684870619712.dkr.ecr.us-west-2.amazonaws.com
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 684870619712.dkr.ecr.us-west-2.amazonaws.com
 
 # update latest version
 docker tag transparent-path/buildly-core:latest 684870619712.dkr.ecr.us-west-2.amazonaws.com/transparent-path/buildly-core:latest
