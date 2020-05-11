813e3b889b3baaa69a416363ee8195f0004be1fd
Greg Lind <gwlind@gmail.com>
2020-05-10 21:13:46 -0400

    update travis for aws

--- scripts/deploy-aws.sh
+++ scripts/deploy-aws.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
-pip install --user awscli # install aws cli w/o sudo
-export PATH=$PATH:$HOME/.local/bin # put aws in the path
-eval $(aws ecr get-login --region us-west-2) #needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars
+
+# Push image to ECR
+###################
+pip install awscli
+aws ecr get-login-password --region us-west-2
+docker login --username AWS --password-stdin 684870619712.dkr.ecr.us-west-2.amazonaws.com
+
+# update latest version
 docker tag buildly:latest 684870619712.dkr.ecr.us-west-2.amazonaws.com/transparent-path/buildly-core:latest
 docker push 684870619712.dkr.ecr.us-west-2.amazonaws.com/transparent-path/buildly-core:latest
