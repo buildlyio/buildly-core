1d4ccc165b5b5259632a02558908a3271fd5f7b2
vishalajackus <73515569+vishalajackus@users.noreply.github.com>
2021-07-16 12:26:12 +0530

    filter consortium by organization (#81)

--- core/views/consortium.py
+++ core/views/consortium.py
@@ -1,11 +1,10 @@
 import logging
 from django_filters.rest_framework import DjangoFilterBackend
-
+from rest_framework.response import Response
 from rest_framework import viewsets
 from core.models import Consortium
 from core.serializers import ConsortiumSerializer
-from core.permissions import AllowOnlyOrgAdmin
-
+from core.permissions import IsSuperUser
 logger = logging.getLogger(__name__)
 
 
@@ -36,9 +35,17 @@ class ConsortiumViewSet(viewsets.ModelViewSet):
     delete:
     Delete a Consortium instance.
     """
+    def list(self, request):
+        queryset = self.filter_queryset(self.get_queryset())
+        organization_uuid = self.request.query_params.get('organization_uuid', None)
+        # It will check if organization uuid in query param
+        if organization_uuid is not None:
+            queryset = queryset.filter(organization_uuids__contains=[organization_uuid])
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
     filter_fields = ('name',)
     filter_backends = (DjangoFilterBackend,)
-    permission_classes = (AllowOnlyOrgAdmin,)
+    permission_classes = (IsSuperUser,)
     queryset = Consortium.objects.all()
     serializer_class = ConsortiumSerializer
