eb50b44dd3b9413174a0027d0c6fe288d9330d85
Radhika Patel <radhika.patel2206@gmail.com>
2024-12-16 14:18:41 +0530

    kapstan deployment changes

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -4,6 +4,17 @@ set -e
 
 bash scripts/tcp-port-wait.sh $DATABASE_HOST $DATABASE_PORT
 
+# export env variable from file
+if [ -e /JWT_PRIVATE_KEY_RSA_BUILDLY ]
+then
+  export JWT_PRIVATE_KEY_RSA_BUILDLY=`cat /JWT_PRIVATE_KEY_RSA_BUILDLY`
+fi
+
+if [ -e /JWT_PUBLIC_KEY_RSA_BUILDLY ]
+then
+  export JWT_PUBLIC_KEY_RSA_BUILDLY=`cat /JWT_PUBLIC_KEY_RSA_BUILDLY`
+fi
+
 echo $(date -u) "- Migrating"
 python manage.py makemigrations
 python manage.py migrate
