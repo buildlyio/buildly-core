48aa41f2c87ef23216e46d40ae1ae7426cde4420
Greg Lind <greg@buildly.io>
2025-02-24 22:15:02 +0000

    fix load init data merge

--- buildly/tests/test_loadinitialdata.py
+++ buildly/tests/test_loadinitialdata.py
@@ -82,8 +82,4 @@ class LoadInitialDataTest(TransactionTestCase):
             == 1
         )
         assert Organization.objects.filter(name=settings.DEFAULT_ORG).count() == 1
-<<<<<<< HEAD
         assert CoreUser.objects.filter(is_superuser=True).count() == 0
-=======
-        assert CoreUser.objects.filter(is_superuser=True).count() == 0
->>>>>>> master
